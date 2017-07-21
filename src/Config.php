1ab0db6370e79404f175a8cdacda2632abb66c22
Hisune <hi@hisune.com>
2017-07-21 11:04:37 +0800

    更新cdn版本到3.6.2
    更新cdn版本到3.6.2

--- src/Config.php
+++ src/Config.php
@@ -10,8 +10,8 @@ namespace Hisune\EchartsPHP;
 
 class Config
 {
-    public static $dist = '//cdn.bootcss.com/echarts/3.4.0';
-    public static $version = '3.4.0';
+    public static $dist = '//cdn.bootcss.com/echarts/3.6.2';
+    public static $version = '3.6.2';
     public static $method = array();
     public static $renderScript = true;
     public static $distType = ''; // Empty is full, other options: simple, common
