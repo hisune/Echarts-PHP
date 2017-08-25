3e1ed089cd6ea78fca0a2100914055150062b53f
Hisune <hi@hisune.com>
2017-08-25 10:02:40 +0800

    dynamic example
    dynamic example

---
+++ demo/dynamic.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by Hisune.
+ * User: hi@hisune.com
+ * Date: 2017/8/25
+ * Time: 09:56
+ */
+
+require('../vendor/autoload.php');
+
+use Hisune\EchartsPHP\ECharts;
+
+if(isset($_GET['ajax']) && $_GET['ajax'] == 'true'):
+    // Your data
+    echo json_encode(['num' => rand(0,100), 'data' => uniqid()]);
+else:
+    // Your data
+    $data = [];
+    $xAxisData = [];
+    for($i = 0; $i < 50; $i++){
+        $data[] = rand(30,70);
+        $xAxisData[] = uniqid();
+    }
+
+    $chart = new ECharts();
+    $chart->tooltip->show = true;
+    $chart->legend->data[] = 'legend data';
+    $chart->xAxis = [
+        'type' => 'category',
+        'data' => $xAxisData,
+    ];
+    $chart->yAxis[] = [
+        'type' => 'value',
+    ];
+    $chart->series[] = [
+        'name' => 'series name',
+        'type' => 'line',
+        'data' => $data
+    ];
+    echo $chart->render('test-id');
+?>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+        setInterval(function(){
+            $.ajax({
+                data: {
+                    'ajax': true
+                },
+                dataType: 'json',
+                success: function(json){
+                    var echart = chart_<?=$chart->getJsVar()?>,
+                        option = echart.getOption();
+
+                    // shift a item then push a new item
+                    option.series[0].data.shift();
+                    option.series[0].data.push(json.num);
+                    option.xAxis[0].data.shift();
+                    option.xAxis[0].data.push(json.data);
+                    echart.setOption({
+                        series: option.series,
+                        xAxis: option.xAxis
+                    });
+                }
+            });
+        }, 1000);
+    </script>
+<?php
+endif;
